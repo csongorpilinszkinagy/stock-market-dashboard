c936f0aa4dc5bab482f32b33afd400ea83229430
Csongor Pilinszki-Nagy <csongor.pilinszkinagy@gmail.com>
2024-08-26 20:15:57 +0000

    Extract timeseries data into a DataFrame

--- data_fetcher.py
+++ data_fetcher.py
@@ -9,3 +9,16 @@ api_key = os.getenv("API_KEY")
 url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey={api_key}'
 r = requests.get(url)
 data = r.json()
+
+time_series_data = data['Time Series (5min)']
+df = pd.DataFrame.from_dict(time_series_data, orient='index') # dict keys are row indexes
+df.columns = ['Open', 'High', 'Low', 'Close', 'Volume'] # rename columns
+df.index = pd.to_datetime(df.index) # convert index to datetime
+df = df.astype({
+    'Open': 'float',
+    'High': 'float',
+    'Low': 'float',
+    'Close': 'float',
+    'Volume': 'int'
+}) # convert prices and volume types
+print(df)
