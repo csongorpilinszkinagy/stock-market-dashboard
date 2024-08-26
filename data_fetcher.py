971b5d634425de7b8c8ec19fb156dd9fe24fafa4
Csongor Pilinszki-Nagy <csongor.pilinszkinagy@gmail.com>
2024-08-26 20:33:04 +0000

    API request caching added

--- data_fetcher.py
+++ data_fetcher.py
@@ -2,13 +2,17 @@ import requests
 from dotenv import load_dotenv
 import os
 import pandas as pd
+import requests_cache
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
 
+session = requests_cache.CachedSession('api_cache', expireexpire_after=300) # Cache expires after 5 minutes
+
 url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey={api_key}'
-r = requests.get(url)
-data = r.json()
+
+response = session.get(url)
+data = response.json()
 
 time_series_data = data['Time Series (5min)']
 df = pd.DataFrame.from_dict(time_series_data, orient='index') # dict keys are row indexes
